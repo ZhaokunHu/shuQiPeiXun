4fa38c47df492546c894eac4ef9f43b1cb3a419d
Zhaokun <aj3023211557@163.com>
2023-07-23 09:58:41 +0800

    编写了ZipReader类

--- JosephReader.py
+++ JosephReader.py
@@ -10,6 +10,7 @@ class TxtReader:
         self.people_list = []
 
     def read_file(self, filename):
+        
         LogMaker.logger.info('读取txt文件')
         try:
             with open(filename, 'r') as txt_file:
@@ -31,6 +32,7 @@ class CsvReader:
         self.people_list = []
 
     def read_file(self):
+        LogMaker.logger.info('读取csv文件')
         try:
             with open(self.filename, 'r', encoding='utf-8') as csv_file:
                 csv_reader = csv.reader(csv_file)
@@ -43,23 +45,29 @@ class CsvReader:
         except FileNotFoundError:
             raise FileNotFoundError(f"Can't find '{self.filename}'")
 
-
-class Reader:
-
-    def read_csv(self, filename):
-        LogMaker.logger.info('读取csv文件')
-
-    def read_file(self, filename):
-        typename = filename.split('.')[1].strip()
-        if (typename == 'txt'):
-            self.read_txt(filename)
-        elif (typename == 'csv'):
-            self.read_csv(filename)
-        elif (typename == 'zip'):
-            LogMaker.logger.info('读取zip文件')
+class ZipReader:
+    def __init__(self, filename):
+        self.filename = filename
+    def read_file(self):
+        LogMaker.logger.info('读取zip文件')
+        try:
             with zipfile.ZipFile(filename, 'r') as zip_file:
-                file_list = zip_file.namelist()
-                self.read_file(file_list[0])
-        else:
-            pass
-        return self.people_list
+                inner_file = zip_file.namelist()[0]
+            return inner_file
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Can't find '{self.filename}'")
+        
+def read_file(self, filename):
+    typename = filename.split('.')[1].strip()
+    if (typename == 'txt'):
+        self.read_txt(filename)
+    elif (typename == 'csv'):
+        self.read_csv(filename)
+    elif (typename == 'zip'):
+        LogMaker.logger.info('读取zip文件')
+        with zipfile.ZipFile(filename, 'r') as zip_file:
+            file_list = zip_file.namelist()
+            self.read_file(file_list[0])
+    else:
+        pass
+    return self.people_list
