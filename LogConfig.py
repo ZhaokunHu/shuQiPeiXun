956e152a331aceea7aeeaf2e456c6b91ab7dc44f
Zhaokun <aj3023211557@163.com>
2023-07-24 13:33:09 +0800

    修改了若干命名

--- LogConfig.py
+++ LogConfig.py
@@ -8,18 +8,18 @@ def setup_logging():
         return logger
     logger.setLevel(logging.DEBUG)
 
-    fh = logging.FileHandler('log.txt', 'w')
-    fh.setLevel(logging.DEBUG)
+    file_hander = logging.FileHandler('log.txt', 'w')
+    file_hander.setLevel(logging.DEBUG)
 
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
+    stream_hander = logging.StreamHandler()
+    stream_hander.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
+    file_hander.setFormatter(formatter)
+    stream_hander.setFormatter(formatter)
 
-    logger.addHandler(fh)
-    logger.addHandler(ch)
+    logger.addHandler(file_hander)
+    logger.addHandler(stream_hander)
     return logger
