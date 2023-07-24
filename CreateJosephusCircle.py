e9804919f31b45a5e3d74ca59ad11f45b8171c05
Zhaokun <aj3023211557@163.com>
2023-07-24 15:09:29 +0800

    将测试的人数增加到10000

--- CreateJosephusCircle.py
+++ CreateJosephusCircle.py
@@ -2,7 +2,7 @@ from faker import Faker
 import random
 fake = Faker("zh_CN")
 file_writer = open('JosephusCircle.txt', 'w')
-for i in range(0, 1000):
+for i in range(0, 10000):
     file_writer.write("序号:{},姓名:{},年龄:{}\n".format(
         i, fake.name(), random.randint(0, 100)))
 file_writer.close()
